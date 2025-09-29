aec1aecf578e516864d1a7299e038a250d8b58cf
Shaminfarhan farhan <shaminfarhanfarhan@gmail.com>
2025-09-29 12:41:27 -0500

    currency conveter function

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { Registration, Login, forgotPassword, ResetPassword } = require("../controllers/userController");
+const { Registration, Login, forgotPassword, ResetPassword, convertprice } = require("../controllers/userController");
 
 const router = express.Router();
 
@@ -10,4 +10,6 @@ router.post("/login", Login);
 router.post("/forgot-password",forgotPassword)
 // 🔑 Reset Password (update with new password)
 router.post("/reset-password/:id/:token", ResetPassword);
+
+router.get("/convert-price",convertprice)
 module.exports = router;
