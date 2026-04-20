d2ee98702f30d30fc088d9edc666b8b1ed334773
Shaminfarhan farhan <shaminfarhanfarhan@gmail.com>
2026-04-20 06:49:35 -0500

    updting main function

--- src/routes/filterRouter.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const { filerDatareferenceNumber } = require("../controllers/filterController");
-const router = express.Router();
-
-router.get("/reference-numbers", filerDatareferenceNumber);
-module.exports = router;    
